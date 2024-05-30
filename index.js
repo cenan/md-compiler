750cef981e03481d56730cec38e5f376f8b9f8ac
cenan <cenan.ozen@gmail.com>
2024-05-30 13:32:10 +0300

    Adds server app

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import unified from "unified";
+import remarkGFM from "remark-gfm";
+import remarkParse from 'remark-parse';
+import remarkSubSuper from 'remark-sub-super';
+import remarkHeadingId from 'remark-heading-autoid';
+
+const app = express();
+app.use(bodyParser.json());
+import cors from "cors";
+app.use(cors({
+	origin: 'http://localhost:4545'
+}));
+
+app.post("/", (req, res) => {
+	const AST = unified()
+		.use(remarkParse)
+		.use(remarkHeadingId)
+		.use(remarkSubSuper)
+		.use(remarkGFM)
+		.parse(req.body.code);
+	res.send(JSON.stringify(AST));
+});
+app.listen(1453);

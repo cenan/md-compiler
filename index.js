a8da74a2ea8ff691f12747eebc65fae5b9882654
cenan <cenan.ozen@gmail.com>
2024-05-30 14:15:02 +0300

    Adds vercel config

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-
 import unified from "unified";
 import remarkGFM from "remark-gfm";
 import remarkParse from 'remark-parse';
@@ -14,6 +13,10 @@ app.use(cors({
 	origin: 'http://localhost:4545'
 }));
 
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
 app.post("/", (req, res) => {
 	const AST = unified()
 		.use(remarkParse)
@@ -23,4 +26,5 @@ app.post("/", (req, res) => {
 		.parse(req.body.code);
 	res.send(JSON.stringify(AST));
 });
-app.listen(1453);
+
+export default app;
--- test.sh
+++ test.sh
@@ -2,5 +2,6 @@
 
 jq -Rs --rawfile cl $1 '{code: $cl}' . > tmp.json
 
-curl localhost:1453 -H "Content-Type: application/json" -X POST --data @tmp.json
+# curl localhost:3000/ -H "Content-Type: application/json" -X POST --data @tmp.json
+curl https://md-compiler.vercel.app/ -H "Content-Type: application/json" -X POST --data @tmp.json
 rm tmp.json
